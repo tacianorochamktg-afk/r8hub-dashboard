8a05d9ffff2ba0cf84ab8d860bb107ba5972e6b9
tacianorochamktg-afk <tacianorochamktg@gmail.com>
2026-06-05 18:39:33 -0300

    fix: remover 'h' acidental na linha setHeader (Content-Type)

--- api/metrics.js
+++ api/metrics.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   // Permitir CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');h
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
